6a3c194840c6d6ce97e33c75fce3b28925c1687f
Eric Sarmiento <ericsarmiento@Eric-Sarmientos-iMac.local>
2014-08-25 19:05:13 -0700

    Initial Commit

---
+++ DemoCommandLine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DemoCommandLine
+//
+//  Created by Eric Sarmiento on 8/25/14.
+//  Copyright (c) 2014 Eric Sarmiento. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
