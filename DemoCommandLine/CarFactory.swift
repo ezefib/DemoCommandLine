1553d4b56f085a9e1588f295a2114e4fbf6d438c
Eric Sarmiento <ericsarmiento@Eric-Sarmientos-iMac.local>
2014-08-28 05:53:56 -0700

    Initial Build

---
+++ DemoCommandLine/CarFactory.swift
@@ -0,0 +1,18 @@
+//
+//  CarFactory.swift
+//  DemoCommandLine
+//
+//  Created by Eric Sarmiento on 8/25/14.
+//  Copyright (c) 2014 Eric Sarmiento. All rights reserved.
+//
+
+import Foundation
+
+class CarFactory{
+    
+    var name = ""
+    var color = ""
+    var horsepower = 0
+    var automaticOption = false
+    
+}
--- DemoCommandLine/main.swift
+++ DemoCommandLine/main.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 
-println("Hello, World!")
 
+var myFirstCar = CarFactory()
+
+myFirstCar.name = "Mustang"
+myFirstCar.color = "Red"
+myFirstCar.horsepower = 200
+myFirstCar.automaticOption = true
+
+println("My \(myFirstCar.name) is \(myFirstCar.color) and has \(myFirstCar.horsepower) horsepowers")
+// $: "My Mustang is Red and has 200 horsepowers"
+
+
+
+var mySecondCar = CarFactory()
+
+mySecondCar.name = "BMW"
+mySecondCar.color = "Blue"
+mySecondCar.horsepower = 300
+mySecondCar.automaticOption = false
+
+println("My \(mySecondCar.name) is \(mySecondCar.color) and has \(mySecondCar.horsepower) horsepowers")
+// $: "My BMW is Blue and has 300 horsepowers"
