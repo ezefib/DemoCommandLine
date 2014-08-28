1b62b465c3a6b6d87b02824567b2dd0fc0d39066
Eric Sarmiento <ericsarmiento@Eric-Sarmientos-iMac.local>
2014-08-28 06:53:05 -0700

    Updated for Unit 2 Lesson 2

--- DemoCommandLine/CarFactory.swift
+++ DemoCommandLine/CarFactory.swift
@@ -13,6 +13,22 @@ class CarFactory{
     var name = ""
     var color = ""
     var horsepower = 0
-    var automaticOption = false
+    var automaticOption = ""
+//    var automaticOption = false
     
+// Unit 2 Lesson 2 - Adding Methods
+    
+    func setupCarDetailsWithName(nameOfCar: String, colorOfCar: String, horsepowerOfCar: Int, automaticOptionOfCar: Bool) {
+        name = nameOfCar
+        color = colorOfCar
+        horsepower = horsepowerOfCar
+        //automaticOption = (automaticOptionOfCar ? "Automatic" : "Manual")
+        automaticOption = (automaticOptionOfCar ? "Automatic" : "Manual")
+    }
+    
+    
+    
+    func description() {
+        println("My \(name) is \(color) and has \(horsepower) horsepowers")
+    }
 }
--- DemoCommandLine/main.swift
+++ DemoCommandLine/main.swift
@@ -11,22 +11,30 @@ import Foundation
 
 var myFirstCar = CarFactory()
 
-myFirstCar.name = "Mustang"
-myFirstCar.color = "Red"
-myFirstCar.horsepower = 200
-myFirstCar.automaticOption = true
+//myFirstCar.name = "Mustang"
+//myFirstCar.color = "Red"
+//myFirstCar.horsepower = 200
+//myFirstCar.automaticOption = true
 
-println("My \(myFirstCar.name) is \(myFirstCar.color) and has \(myFirstCar.horsepower) horsepowers")
+myFirstCar.setupCarDetailsWithName("Mustang", colorOfCar: "Red", horsepowerOfCar: 200, automaticOptionOfCar: true)
+
+//Unit 2 Lesson 1
+//println("My \(myFirstCar.name) is \(myFirstCar.color) and has \(myFirstCar.horsepower) horsepowers")
 // $: "My Mustang is Red and has 200 horsepowers"
 
+myFirstCar.description()
+// $: "My Mustang is Red and has 200 horsepowers and is Automatic"
 
 
 var mySecondCar = CarFactory()
 
-mySecondCar.name = "BMW"
-mySecondCar.color = "Blue"
-mySecondCar.horsepower = 300
-mySecondCar.automaticOption = false
+//mySecondCar.name = "BMW"
+//mySecondCar.color = "Blue"
+//mySecondCar.horsepower = 300
+//mySecondCar.automaticOption = false
 
-println("My \(mySecondCar.name) is \(mySecondCar.color) and has \(mySecondCar.horsepower) horsepowers")
+mySecondCar.setupCarDetailsWithName("BMW", colorOfCar: "Blue", horsepowerOfCar: 300, automaticOptionOfCar: false)
+
+//println("My \(mySecondCar.name) is \(mySecondCar.color) and has \(mySecondCar.horsepower) horsepowers")
 // $: "My BMW is Blue and has 300 horsepowers"
+mySecondCar.description()
